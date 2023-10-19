3627299fb51bc3a5c0c1a4f30ecb6f65a880d63e
Andrew <ondralukes06@seznam.cz>
2023-10-19 07:22:41 +0200

    ALL unit scenes became corrupted

--- scenes/screens/levels/units/knight.gd
+++ scenes/screens/levels/units/knight.gd
@@ -4,6 +4,7 @@ extends MeleeUnit
 func get_boost():
 #	$movement_comp.remain_movement += 1
 	print("KNIGHT SHOULD GET ONE MORE ACTION ", self,     )
+	$movement_comp.remain_distance = $movement_comp.base_action_range 
 #	print("KNIGHT IS GETTING ONE MORE MOVE ", self, " ",   )
 	
 func _ready():
