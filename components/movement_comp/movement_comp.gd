e31886f251d10cfc80e499bfed27230f3b21c605
Andrew <ondralukes06@seznam.cz>
2023-10-23 07:10:33 +0200

    terrain movement moves with movement component

--- components/movement_comp/movement_comp.gd
+++ components/movement_comp/movement_comp.gd
@@ -1,6 +1,5 @@
 class_name MovementComponent
 extends Node2D
-#signal remain_movement_changed( )
 signal ran_out_of_movement()
 signal hit_river()
 var base_movement_range:int:
@@ -18,23 +17,14 @@ var remain_distance  = base_movement_range:
 			owner.update_stats_bar()
 	get:
 		return remain_distance
-#var movement_modifiers:Dictionary = {
-#	"base_modifier": 1,
-#	"on_road": 0,
-#	"in_forrest": 0,
-#	"in_town": 0, 
-#} 
+ 
 var current_movement_modifier:float = 1.0   
-#var on_bridge:= false 
-#var on_river:= false
  
 func _ready():
 	await owner._ready()
-#	$"terrain_type_finder".find_current_overlapping_terrain()
-#	current_movement_modifier= call_deferred_thread_group("translate_terrain_to_move_modifier") 
-#	call_deferred_thread_group("calculate_total_movement_modifier")
 	global_start_turn_position = owner.center
-
+func _process(_delta):
+	$terrain_type_finder.global_position = owner.center
 func translate_terrain_to_move_modifier() -> float:
 	$"terrain_type_finder".find_current_overlapping_terrain()
 	match $"terrain_type_finder".overlapping_terrain_type:
@@ -47,7 +37,7 @@ func translate_terrain_to_move_modifier() -> float:
 			return 0.5 
 		"town":
 			return 0.75 
-	print("THERE IS A PROBLEM IN THE TRANSLATE TO MOVE MODIFIER FC ", $"../../terrain_type_finder".overlapping_terrain_type)
+	print("THERE IS A PROBLEM IN THE TRANSLATE TO MOVE MODIFIER FC ", $"terrain_type_finder".overlapping_terrain_type)
 	return 10000
  
  
--- scenes/screens/levels/units/knight.gd
+++ scenes/screens/levels/units/knight.gd
@@ -2,11 +2,13 @@ class_name Knight
 extends MeleeUnit
 
 func get_boost():
-#	$movement_comp.remain_movement += 1
 	print("KNIGHT SHOULD GET ONE MORE ACTION ", self,     )
-	#$movement_comp.remain_distance = $movement_comp.base_action_range 
-#	print("KNIGHT IS GETTING ONE MORE MOVE ", self, " ",   )
-	
+	$movement_comp.remain_distance = $movement_comp.base_movement_range
+	$HorseNeighing.play()
+	action_component.remain_actions += 1
+	var tween = get_tree().create_tween()
+	tween.tween_property($ColorRect, "modulate", Color(1,1,1), 0.2)
+	tween.tween_property($ColorRect, "modulate",   color, 0.2)
 func _ready():
 	super._ready()
 	unit_name = "Knight"
